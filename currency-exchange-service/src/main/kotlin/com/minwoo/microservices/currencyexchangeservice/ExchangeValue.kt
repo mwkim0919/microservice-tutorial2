144a189fa25ffd76db11bf4fca812dcd387bd8fd
minwoo <mwkim0919@gmail.com>
2019-12-07 20:38:30 -0800

    start of currency-exchange-service and adding hard-coded ExchangeValue

---
+++ currency-exchange-service/src/main/kotlin/com/minwoo/microservices/currencyexchangeservice/ExchangeValue.kt
@@ -0,0 +1,10 @@
+package com.minwoo.microservices.currencyexchangeservice
+
+import java.math.BigDecimal
+
+data class ExchangeValue(
+  val id: Long,
+  val from: String,
+  val to: String,
+  val conversionMultiple: BigDecimal
+)
