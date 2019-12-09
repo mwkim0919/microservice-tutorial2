bf4aeb5efd29d8976b915bd3a188e75011b5c060
minwoo <mwkim0919@gmail.com>
2019-12-08 22:38:57 -0800

    invoking currency-exchange-service from currency-conversion-service

--- currency-conversion-service/src/main/kotlin/com/minwoo/microservices/currencyconversionservice/CurrencyConversionBean.kt
+++ currency-conversion-service/src/main/kotlin/com/minwoo/microservices/currencyconversionservice/CurrencyConversionBean.kt
@@ -3,20 +3,11 @@ package com.minwoo.microservices.currencyconversionservice
 import java.math.BigDecimal
 
 data class CurrencyConversionBean (
-  val id: Long,
-  val from: String,
-  val to: String,
-  val conversionMultiple: BigDecimal,
-  val quantity: BigDecimal,
-  val totalCalculatedAmount: BigDecimal,
+  val id: Long?,
+  val from: String?,
+  val to: String?,
+  val conversionMultiple: BigDecimal?,
+  val quantity: BigDecimal?,
+  val totalCalculatedAmount: BigDecimal?,
   val port: Int?
-) {
-  private constructor(): this(
-    0,
-    "",
-    "",
-    BigDecimal.ZERO,
-    BigDecimal.ZERO,
-    BigDecimal.ZERO,
-    0)
-}
+)
--- currency-conversion-service/src/main/kotlin/com/minwoo/microservices/currencyconversionservice/CurrencyConversionController.kt
+++ currency-conversion-service/src/main/kotlin/com/minwoo/microservices/currencyconversionservice/CurrencyConversionController.kt
@@ -3,6 +3,7 @@ package com.minwoo.microservices.currencyconversionservice
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestTemplate
 import java.math.BigDecimal
 
 @RestController
@@ -12,8 +13,30 @@ class CurrencyConversionController {
   fun convertCurrency(
     @PathVariable from: String,
     @PathVariable to: String,
-    @PathVariable quantity: BigDecimal): CurrencyConversionBean {
-    val currencyConversionBean = CurrencyConversionBean(1L, from, to, BigDecimal.ONE, quantity, BigDecimal.ONE, 0)
+    @PathVariable quantity: BigDecimal): CurrencyConversionBean? {
+
+    val uriVariables = HashMap<String, String>()
+    uriVariables["from"] = from
+    uriVariables["to"] = to
+
+    val responseEntity = RestTemplate().getForEntity(
+      "http://localhost:8000/currency-exchange/from/${from}/to/${to}",
+      CurrencyConversionBean::class.java,
+      uriVariables)
+
+    val response = responseEntity.body
+
+    val currencyConversionBean = response?.id?.let {
+      CurrencyConversionBean(
+        it,
+        from,
+        to,
+        response.conversionMultiple,
+        quantity,
+        quantity.multiply(response.conversionMultiple),
+        0
+      )
+    }
     return currencyConversionBean
   }
 }
