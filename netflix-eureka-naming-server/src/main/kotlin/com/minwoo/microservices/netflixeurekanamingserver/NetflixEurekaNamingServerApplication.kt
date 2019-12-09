393d6cbcf677c9e051725a63e741a54def194e09
minwoo <mwkim0919@gmail.com>
2019-12-08 23:35:52 -0800

    setting up eureka naming server

--- netflix-eureka-naming-server/src/main/kotlin/com/minwoo/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.kt
+++ netflix-eureka-naming-server/src/main/kotlin/com/minwoo/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.kt
@@ -2,10 +2,12 @@ package com.minwoo.microservices.netflixeurekanamingserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 class NetflixEurekaNamingServerApplication
 
 fun main(args: Array<String>) {
-	runApplication<NetflixEurekaNamingServerApplication>(*args)
+  runApplication<NetflixEurekaNamingServerApplication>(*args)
 }
