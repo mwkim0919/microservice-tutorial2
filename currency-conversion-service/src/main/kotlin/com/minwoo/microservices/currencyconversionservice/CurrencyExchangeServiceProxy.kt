81c7f47b1df75a6a9d1969c30bd811238d1c78b2
minwoo <mwkim0919@gmail.com>
2019-12-08 23:20:28 -0800

    setting up client side load balancing with Ribbon for currency-conversion-service

--- currency-conversion-service/src/main/kotlin/com/minwoo/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.kt
+++ currency-conversion-service/src/main/kotlin/com/minwoo/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.kt
@@ -1,10 +1,12 @@
 package com.minwoo.microservices.currencyconversionservice
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 
-@FeignClient(name = "currency-exchange-service", url="localhost:8000")
+@FeignClient(name = "currency-exchange-service")
+@RibbonClient(name = "currency-exchange-service")
 interface CurrencyExchangeServiceProxy {
   @GetMapping("/currency-exchange/from/{from}/to/{to}")
   fun retrieveExchangeValue(@PathVariable("from") from: String, @PathVariable("to") to: String): CurrencyConversionBean
