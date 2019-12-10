735deb6cabb522a76edf023d7def60cfa6df27ef
minwoo <mwkim0919@gmail.com>
2019-12-09 19:45:43 -0800

    connecting currency-exchange-service and limits-service to eureka

--- currency-exchange-service/src/main/kotlin/com/minwoo/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.kt
+++ currency-exchange-service/src/main/kotlin/com/minwoo/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.kt
@@ -2,8 +2,10 @@ package com.minwoo.microservices.currencyexchangeservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class CurrencyExchangeServiceApplication
 
 fun main(args: Array<String>) {
