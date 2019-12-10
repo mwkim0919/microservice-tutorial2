c4030e93144895b8988255fc18680dcaadd3fa65
minwoo <mwkim0919@gmail.com>
2019-12-09 20:34:38 -0800

    adding zuul-api-gateway-server package

---
+++ netflix-zuul-api-gateway-server/src/main/kotlin/com/minwoo/microservices/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.kt
@@ -0,0 +1,11 @@
+package com.minwoo.microservices.netflixzuulapigatewayserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NetflixZuulApiGatewayServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<NetflixZuulApiGatewayServerApplication>(*args)
+}
