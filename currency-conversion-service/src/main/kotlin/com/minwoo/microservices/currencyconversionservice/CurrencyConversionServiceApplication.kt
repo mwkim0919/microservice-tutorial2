c2c77c647c15b1accc2caa4d84ead8369e5091e2
minwoo <mwkim0919@gmail.com>
2019-12-09 19:38:17 -0800

    connecting currency-conversion-service to eureka

--- currency-conversion-service/src/main/kotlin/com/minwoo/microservices/currencyconversionservice/CurrencyConversionServiceApplication.kt
+++ currency-conversion-service/src/main/kotlin/com/minwoo/microservices/currencyconversionservice/CurrencyConversionServiceApplication.kt
@@ -2,10 +2,12 @@ package com.minwoo.microservices.currencyconversionservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
 @EnableFeignClients("com.minwoo.microservices.currencyconversionservice")
+@EnableDiscoveryClient
 class CurrencyConversionServiceApplication
 
 fun main(args: Array<String>) {
